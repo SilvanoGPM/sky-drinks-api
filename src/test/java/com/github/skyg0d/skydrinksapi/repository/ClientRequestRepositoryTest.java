1acbb4eb3ad1d667f19e522e8683f04f40e54fbf
SkyG0D <silvanosilvino@hotmail.com>
2021-11-20 01:52:28 -0300

    test: tests for ClientRequestRepository created

---
+++ src/test/java/com/github/skyg0d/skydrinksapi/repository/ClientRequestRepositoryTest.java
@@ -0,0 +1,110 @@
+package com.github.skyg0d.skydrinksapi.repository;
+
+import com.github.skyg0d.skydrinksapi.domain.ClientRequest;
+import com.github.skyg0d.skydrinksapi.domain.Drink;
+import com.github.skyg0d.skydrinksapi.domain.Table;
+import com.github.skyg0d.skydrinksapi.repository.drink.DrinkRepository;
+import com.github.skyg0d.skydrinksapi.repository.request.ClientRequestRepository;
+import com.github.skyg0d.skydrinksapi.repository.table.TableRepository;
+import com.github.skyg0d.skydrinksapi.util.drink.DrinkCreator;
+import com.github.skyg0d.skydrinksapi.util.request.ClientRequestCreator;
+import com.github.skyg0d.skydrinksapi.util.table.TableCreator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@DisplayName("Tests for ClienteRequestRepository")
+class ClientRequestRepositoryTest {
+
+    @Autowired
+    private ClientRequestRepository clientRequestRepository;
+
+    @Autowired
+    private DrinkRepository drinkRepository;
+
+    @Autowired
+    private TableRepository tableRepository;
+
+    @Test
+    @DisplayName("save persist client request when successful")
+    void save_PersistClientRequest_WhenSuccessful() {
+        ClientRequest requestToBeSave = ClientRequestCreator.createClientRequestToBeSave();
+
+        ClientRequest requestSaved = clientRequestRepository.save(requestToBeSave);
+
+        assertThat(requestSaved).isNotNull();
+
+        assertThat(requestSaved.getTotalPrice())
+                .isNotNull()
+                .isEqualTo(requestToBeSave.getTotalPrice());
+
+        assertThat(requestSaved.getDrinks())
+                .isNotEmpty()
+                .contains(DrinkCreator.createValidDrink());
+
+        assertThat(requestSaved.getTable())
+                .isNotNull()
+                .isEqualTo(TableCreator.createValidTable());
+    }
+
+    @Test
+    @DisplayName("save updates client request when successful")
+    void save_UpdateClientRequest_WhenSuccessful() {
+        Drink drinkSaved = drinkRepository.save(DrinkCreator.createDrinkToBeSave());
+
+        Table tableSaved = tableRepository.save(TableCreator.createTableToBeSave());
+
+        ClientRequest requestToBeSave = ClientRequestCreator.createClientRequestToBeSave();
+
+        requestToBeSave.setDrinks(new ArrayList<>(List.of(drinkSaved)));
+
+        requestToBeSave.setTable(tableSaved);
+
+        ClientRequest requestSaved = clientRequestRepository.save(requestToBeSave);
+
+        requestSaved.setTotalPrice(DrinkCreator.createValidDrink().getPrice());
+        requestSaved.setFinished(true);
+
+        ClientRequest requestUpdated = clientRequestRepository.save(requestSaved);
+
+        assertThat(requestUpdated).isNotNull();
+
+        assertThat(requestUpdated.isFinished()).isEqualTo(requestSaved.isFinished());
+
+        assertThat(requestUpdated.getTotalPrice()).isEqualTo(requestSaved.getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("delete remove client request when successful")
+    void delete_RemoveClientRequest_WhenSuccessful() {
+        ClientRequest requestToBeSave = ClientRequestCreator.createClientRequestToBeSave();
+
+        ClientRequest requestSaved = clientRequestRepository.save(requestToBeSave);
+
+        clientRequestRepository.delete(requestSaved);
+
+        Optional<ClientRequest> requestFound = clientRequestRepository.findById(requestSaved.getUuid());
+
+        assertThat(requestFound).isEmpty();
+    }
+
+    @Test
+    @DisplayName("save throws ConstraintViolationException when client request properties is invalid")
+    void save_ThrowsConstraintViolationException_WhenClientRequestPropertiesIsInvalid() {
+        ClientRequest request = new ClientRequest();
+
+        assertThatExceptionOfType(ConstraintViolationException.class)
+                .isThrownBy(() -> clientRequestRepository.saveAndFlush(request));
+
+    }
+
+}
---
+++ src/test/java/com/github/skyg0d/skydrinksapi/util/request/ClientRequestCreator.java
@@ -0,0 +1,47 @@
+package com.github.skyg0d.skydrinksapi.util.request;
+
+import com.github.skyg0d.skydrinksapi.domain.ClientRequest;
+import com.github.skyg0d.skydrinksapi.util.drink.DrinkCreator;
+import com.github.skyg0d.skydrinksapi.util.table.TableCreator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ClientRequestCreator {
+
+    private static final UUID uuid = UUID.fromString("da278fd9-cc0a-464b-97ad-9a5215fb8fcf");
+
+    public static ClientRequest createClientRequestToBeSave() {
+        return ClientRequest
+                .builder()
+                .table(TableCreator.createValidTable())
+                .drinks(new ArrayList<>(List.of(DrinkCreator.createValidDrink())))
+                .totalPrice(0.0)
+                .finished(false)
+                .build();
+    }
+
+    public static ClientRequest createValidClientRequest() {
+        return ClientRequest
+                .builder()
+                .uuid(uuid)
+                .drinks(new ArrayList<>(List.of(DrinkCreator.createValidDrink())))
+                .table(TableCreator.createValidTable())
+                .totalPrice(0.0)
+                .finished(false)
+                .build();
+    }
+
+    public static ClientRequest createUpdatedClientRequest() {
+        return ClientRequest
+                .builder()
+                .uuid(uuid)
+                .drinks(new ArrayList<>(List.of(DrinkCreator.createValidDrink())))
+                .table(TableCreator.createValidTable())
+                .totalPrice(DrinkCreator.createValidDrink().getPrice())
+                .finished(true)
+                .build();
+    }
+
+}
