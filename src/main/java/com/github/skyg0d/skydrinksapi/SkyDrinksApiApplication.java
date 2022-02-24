f4c37d00a95730ca16173c064113ca642975d1be
SkyG0D <silvanosilvino@hotmail.com>
2022-02-23 23:52:04 -0300

    chore: version of spring maven

--- src/main/java/com/github/skyg0d/skydrinksapi/SkyDrinksApiApplication.java
+++ src/main/java/com/github/skyg0d/skydrinksapi/SkyDrinksApiApplication.java
@@ -2,7 +2,6 @@ package com.github.skyg0d.skydrinksapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SkyDrinksApiApplication {
